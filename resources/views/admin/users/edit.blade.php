88db6c7b0a89ee838c3f6c20378616d213712f6c
vagrant <vagrant@homestead>
2018-04-10 10:13:09 +0000

    Users section is working except security

---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.admin')
+
+
+
+@section('content')
+
+    <h2>Edit Users</h2>
+
+    <div class="row">
+
+    <div class="col-sm-3">
+
+        <img src="{{$user->photo ? $user->photo->file : 'http://via.placeholder.com/400x400'}}" alt="" class="img-responsive img-rounded">
+
+    </div>
+
+    <div class="col-sm-9">
+
+
+        {!! Form::model($user, ['method'=>'PATCH', 'action' => ['AdminUsersController@update', $user->id], 'files'=>true]) !!}
+
+        <div class="form-group">
+
+            {!! Form::label('name', 'Name') !!}
+            {!! Form::text('name', null, ['class'=>'form-control']) !!}
+
+        </div>
+
+        <div class="form-group">
+
+            {!! Form::label('email', 'Email') !!}
+            {!! Form::email('email', null, ['class'=>'form-control']) !!}
+
+        </div>
+
+        <div class="form-group">
+
+            {!! Form::label('role_id', 'Role:') !!}
+            {!! Form::select('role_id',  $roles , null, ['class'=>'form-control']) !!}
+
+        </div>
+
+        <div class="form-group">
+
+            {!! Form::label('is_active', 'Status') !!}
+            {!! Form::select('is_active', array(1=>'Active',2=>'Not Active'), null, ['class'=>'form-control']) !!}
+
+        </div>
+
+        <div class="form-group">
+
+            {!! Form::label('photo_id', 'Photo:') !!}
+            {!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
+
+        </div>
+
+
+        <div class="form-group">
+
+            {!! Form::label('password', 'Password') !!}
+            {!! Form::password('password', ['class'=>'form-control']) !!}
+
+        </div>
+
+        <div class="form-group">
+
+            {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+
+        </div>
+
+        {!! Form::close() !!}
+
+    </div>
+
+    </div>
+
+    <div class="row">
+    @include('includes.form_error')
+    </div>
+
+@endsection
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -9,6 +9,7 @@
          <thead>
            <tr>
                <th>ID</th>
+               <th>Photo</th>
                <th>Name</th>
                <th>Email</th>
                <th>Role</th>
@@ -24,7 +25,8 @@
              @foreach($users as $user)
            <tr>
              <td>{{$user->id}}</td>
-             <td>{{$user->name}}</td>
+             <td><img height="30" src="{{$user->photo ? $user->photo->file : 'http://via.placeholder.com/50x50'}}" alt=""></td>
+             <td><a href="{{route('users.edit', $user->id)}}">{{$user->name}}</a></td>
              <td>{{$user->email}}</td>
              <td>{{$user->role->name}}</td>
              <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
