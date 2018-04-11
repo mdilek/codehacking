8245f3018118615d801bc5e85ef070d62f2572bb
vagrant <vagrant@homestead>
2018-04-11 09:17:26 +0000

    able to create posts with static category

--- app/User.php
+++ app/User.php
@@ -40,4 +40,24 @@ class User extends Authenticatable
     }
 
 
+    public function isAdmin(){
+
+        if($this->role->name == 'Administrator'){
+
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+    public function posts(){
+
+
+        return $this->hasMany('App\Post');
+
+    }
+
+
 }
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+
+    <h1 class="text-center">Opps Page Not Available</h1>
+
+@endsection
+
